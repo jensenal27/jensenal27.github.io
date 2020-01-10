a6c3d4776c2b06ea4ddc3ef8441f70b219c38721
jensenal27 <48623977+jensenal27@users.noreply.github.com>
2020-01-10 01:35:20 -0500

    contact form style changes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -77,25 +77,25 @@ const Contact = ({inView}) => {
     const onSubmit = () => {
         const isValid = validate();
         if (isValid) {
-            // setIsSent(true);
-            fetch('https://agile-plains-44092.herokuapp.com/send', {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    name: name,
-                    company: company,
-                    email: email,
-                    phone: phone,
-                    message: message
-                })
-            })
-            .then(function(response) {
-                if (response.status !== 200) {
-                    throw new Error('Not 200 Response')
-                } else {
-                    setIsSent(true)
-                }
-            }).catch(err => console.log(err));
+            setIsSent(true);
+            // fetch('https://agile-plains-44092.herokuapp.com/send', {
+            //     method: 'post',
+            //     headers: {'Content-Type': 'application/json'},
+            //     body: JSON.stringify({
+            //         name: name,
+            //         company: company,
+            //         email: email,
+            //         phone: phone,
+            //         message: message
+            //     })
+            // })
+            // .then(function(response) {
+            //     if (response.status !== 200) {
+            //         throw new Error('Not 200 Response')
+            //     } else {
+            //         setIsSent(true)
+            //     }
+            // }).catch(err => console.log(err));
         }
     }
     
@@ -113,16 +113,31 @@ const Contact = ({inView}) => {
                         <div className = 'contactform'>
                             <div className = 'contacttitle'>CONTACT ME</div>
                             <div className = 'contactsubtitle'>jensenal27@gmail.com | (804)-335-6054</div>
-                            <form method="POST" action="send">
-                                <input onChange={onNameChange} type="text" name="name" id="name" placeholder="Name"/>
-                                <div className = {nameError === '' ? 'invalid' : 'invalid-show'} id = 'invname'>{nameError}</div>
-                                <input onChange={onCompanyChange} type="text" name="company" id="company" placeholder="Company"/>
-                                <div className = {companyError === '' ? 'invalid' : 'invalid-show'} id = 'invcompany'>{companyError}</div>
-                                <input onChange={onEmailChange} type="email" name="email" id="email" placeholder="Email Address"/>
-                                <div className = {emailError === '' ? 'invalid' : 'invalid-show'} id = 'invemail'>{emailError}</div>
-                                <input onChange={onPhoneChange} type="text" name="phone" id="phone" placeholder="Phone Number"/>
-                                <textarea onChange={onMessageChange} name="message" id="message" placeholder="Type your message here..." rows="3"></textarea>
-                                <div className = {messageError === '' ? 'invalid' : 'invalid-show'} id = 'invmessage'>{messageError}</div>
+                            <form method="POST" action="send" novalidate="novalidate">
+                                <div className = 'formsection' id='namewrap'>
+                                    <input onChange={onNameChange} type="text" name="name" id="name" required/>
+                                    <label id='namelabel'>Name</label>
+                                    <div className = {nameError === '' ? 'invalid' : 'invalid-show'} id = 'invname'>{nameError}</div>
+                                </div>
+                                <div className = 'formsection' id='companywrap'>
+                                    <input onChange={onCompanyChange} type="text" name="company" id="company" required/>
+                                    <label id='companylabel'>Company</label>
+                                    <div className = {companyError === '' ? 'invalid' : 'invalid-show'} id = 'invcompany'>{companyError}</div>
+                                </div>
+                                <div className = 'formsection' id='emailwrap'>
+                                    <input onChange={onEmailChange} type="text" name="email" id="email" required/>
+                                    <label>Email Address</label>
+                                    <div className = {emailError === '' ? 'invalid' : 'invalid-show'} id = 'invemail'>{emailError}</div>
+                                </div>
+                                <div className = 'formsection' id='phonewrap'>
+                                    <input onChange={onPhoneChange} type="text" name="phone" id="phone" required/>
+                                    <label>Phone Number</label>
+                                </div>
+                                <div className = 'formsection' id='messagewrap'>
+                                    <textarea onChange={onMessageChange} name="message" id="message" required></textarea>
+                                    <label>Type your message here...</label>
+                                    <div className = {messageError === '' ? 'invalid' : 'invalid-show'} id = 'invmessage'>{messageError}</div>
+                                </div>
                                 <div onClick={onSubmit} className = 'button'>SUBMIT</div>
                             </form>
                         </div>
